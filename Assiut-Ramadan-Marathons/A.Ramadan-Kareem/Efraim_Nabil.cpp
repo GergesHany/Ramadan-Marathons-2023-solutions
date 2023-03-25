0b444510cc6020600e2db7d03ece0d32d5c426b2
Efraim Nabil <efraimefefraim@gmail.com>
2023-03-25 18:14:08 +0200

    Add Efraim Nabil solution

---
+++ Assiut-Ramadan-Marathons/A.Ramadan-Kareem/Efraim_Nabil.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define OO 1e9
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.ren()
+#define TC int t; cin >> t; while(t--)
+using namespace std;
+void Fero(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+    #endif
+}
+
+int main(){
+    Fero();
+
+    cout << "Ramadan Kareem";
+
+    return 0;
+}
+
