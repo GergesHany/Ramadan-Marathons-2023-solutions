042703f11126312d52874136886b629f01b3b92f
Efraim Nabil <efraimefefraim@gmail.com>
2023-03-25 18:48:01 +0200

    Add efraim solution

---
+++ Minya-Ramadan-Marathons/B.A-gift/Efraim_Nabil.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define OO 1e9
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.ren()
+#define TC int t; cin >> t; while(t--)
+using namespace std;
+void Fero(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+    #endif
+}
+
+int main(){
+    Fero();
+
+    TC{
+        ll n;
+        cin >> n;
+        cout << (n / 4) * 3 + (n % 4) << endl;
+    }
+
+    return 0;
+}
