190676854935b57db798ef61144aae5560193095
Gerges Hany <gergeshany179@gmail.com>
2023-03-27 00:05:50 +0200

    Gerges Hany solution

---
+++ SCU-Ramadan-Challenges/B.A-Strong-Company/GergesHany.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std; 
+using namespace __gnu_pbds;
+ 
+#define EPS 1e-8
+#define sz(s) (ll)(s.size())  
+#define rall(s)  s.rbegin(), s.rend()
+#define TC int t; cin >> t; while(t--)
+#define getline(s) getline(cin >> ws, s)
+#define all(vec)  vec.begin(),  vec.end()
+#define updmin(a, b) a = min((ll)a, (ll)b)
+#define updmax(a, b) a = max((ll)a, (ll)b)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define endd(s) return void(cout << s << "\n")
+#define fixed(n) cout << fixed << setprecision(n)
+#define ceill(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define cin_2d(vec, n, m) for(int i=0;i<n;i++) for(int j=0;j<m&&cin>>vec[i][j];j++);
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define ordered_set tree<ll, null_type, less <ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define multi_ordered_set tree<ll, null_type, less_equal <ll>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1e9 + 7;
+const ll INF = 1LL << 60;
+typedef unsigned long long ull;
+ 
+ 
+void Gerges_Hany(){
+  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  #ifndef ONLINE_JUDGE  
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+  #endif
+}
+ 
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x; return in;
+}
+ 
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; return out;
+}
+
+// Modulo multiplicative inverse
+template < typename T = int > struct Modulo_multiplicative_inverse{
+ 
+  Modulo_multiplicative_inverse() = default;
+ 
+  T n, r, Mod;
+  vector < T > fact, inv_fact;
+ 
+  auto Take_mod(T a, T b) -> ll {
+     return (a % mod * b % mod) % mod;
+  };
+ 
+  Modulo_multiplicative_inverse(T n, T r, T Mod) : n(n), r(r), Mod(Mod) {
+    fact = vector < T > (n + 1, 1);
+    inv_fact = vector < T > (n + 1, 1);
+    for (T i = 1; i <= n; i++) {
+      fact[i] = Take_mod(fact[i - 1], i);
+      inv_fact[i] = Modulo_Inverse(fact[i]);
+    }
+  }
+ 
+  T fast_power(T a, T b){
+    T res = 1;
+    while (b){
+      if (b & 1) res = Take_mod(res, a);
+      a = Take_mod(a, a);
+      b >>= 1;
+    }
+    return res;
+  }
+  
+  // fact[n] / (fact[r] * fact[n - r]) = fact[n] * (fact[r] * fact[n - r])^-1 = fact[n] * inv_fact[r] * inv_fact[n - r]
+  T nCr(T n, T r){
+    if (n < r) return 0;
+    return Take_mod(fact[n], Take_mod(inv_fact[r], inv_fact[n - r]));
+  }
+ 
+  // fact[n] / fact[n - r] = fact[n] * (fact[n - r])^-1 = fact[n] * inv_fact[n - r] 
+  T nPr(T n, T r){
+    if (n < r) return 0;
+    return Take_mod(fact[n], inv_fact[n - r]);
+  }
+ 
+  T Modulo_Inverse(T a){
+    return fast_power(a, Mod - 2);
+  }
+ 
+};
+
+Modulo_multiplicative_inverse < ll > ncr(1e6 + 10, 1e6 + 10, mod);
+void Accepted(){  
+ 
+  ll n, k;
+  cin >> n >> k;
+  ll ans = 0;
+  for (ll i = 1; i <= n; i++){
+    ans += ncr.nCr(k, i);
+    ans %= mod;
+  }
+
+  cout << ans << "\n";
+
+}
+ 
+int main()
+{ 
+ 
+  Gerges_Hany();
+  int testcaces = 1;
+   cin >> testcaces;
+  while (testcaces--){  
+    // cout << "Case #" << T++ << ": ";
+    Accepted();
+  }   
+  Time;
+  return 0;
+}
