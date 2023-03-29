80fcd3228a3a8f64c52e2f68dfe5c5d154a9b98d
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-29 16:58:57 +0200

    Add day4 in Assiut

---
+++ Assiut-Ramadan-Marathons/D.Ramadan-sequence/Ahmed_Hossam.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+// This function finds the length of the longest consecutive subarray 
+// that adds up to a given integer n
+void Solve(){
+    // Initializing variables
+    ll n;
+    cin >> n;
+    ll l = 1, r = 1, ans = 0, sum = 0;
+    // Using a sliding window technique to find the longest consecutive subarray
+    while(r <= n){
+        sum += r;
+        while(sum > n)
+            sum -= l++;
+        if(sum == n)
+            ans = max(ans, r - l + 1);
+        r++;
+    }
+    // Outputting the length of the longest consecutive subarray
+    cout << ans << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    // cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";
+        Solve();
+    }
+    return 0;
+}
