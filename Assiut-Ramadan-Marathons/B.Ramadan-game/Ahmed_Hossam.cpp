3cb58efdd640570b02f9dfac6c057c6a506034e2
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-29 16:56:09 +0200

    Add day2 in Assiut

---
+++ Assiut-Ramadan-Marathons/B.Ramadan-game/Ahmed_Hossam.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+// Define a function named Solve
+void Solve(){
+    // Declare two double variables, h and m to store hours and minutes respectively
+    double h, m;
+    // Declare a char variable, dot to store the colon between hours and minutes in the input
+    char dot;
+    // Read input from the user and store it in h, dot and m
+    cin >> h >> dot >> m;
+    // Calculate the angle between the hour and minute hands of a clock using the formula |30*h + 0.5*m - 6*m|
+    double angle = abs(30 * h + 0.5 * m - 6 * m);
+    // If the angle is greater than 180 degrees, then subtract it from 360 to get the smaller angle
+    if (angle > 180) angle = 360 - angle;
+    // Print the angle with 6 decimal places using cout
+    cout << fixed(6) << angle << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    // cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";
+        Solve();
+    }
+    return 0;
+}
