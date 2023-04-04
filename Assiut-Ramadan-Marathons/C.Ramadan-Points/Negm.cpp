da24e2ef6cc9c55321bb074e0e71bdbb1f4116c2
Ahmed Negm <91202410+AhmedNegm5@users.noreply.github.com>
2023-04-04 14:16:56 +0200

    Negm solution C

---
+++ Assiut-Ramadan-Marathons/C.Ramadan-Points/Negm.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define OO 2'000'000'000
+#define ull unsigned long long
+#define nl '\n'
+#define sz(x) (ll)(x.size())
+#define all(x) x.begin(),x.end()
+#define rall(s)  s.rbegin(), s.rend()
+#define getline(s) getline(cin>>ws,s)
+#define ceill(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define pi  3.141592653589793
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define multi_ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+ 
+void Fast_IO(){
+ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+// freopen("filename.in", "r", stdin);
+// freopen("filename.txt", "w", stdout);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+#endif
+}
+
+
+
+
+int dx[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+
+
+
+void solve(){
+int n; cin>>n;
+vector<ll> v(n);
+for(auto &i:v)cin>>i;
+sort(all(v));
+vector<ll>ans;
+for(int i=0;i<n;i++){
+    if(i+1<n) ans.emplace_back(v[i+1]-v[i]);
+    if(i+2<n) ans.emplace_back(v[i+2]-v[i]);
+}
+sort(all(ans));
+cout<<ans[2]<<nl;
+
+}
+ 
+int main(){
+    Fast_IO();
+int t =1; 
+//cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}  
