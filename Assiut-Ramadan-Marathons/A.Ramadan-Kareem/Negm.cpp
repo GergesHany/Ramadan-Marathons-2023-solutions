4eec2c06372b803e079a8df2eab9b7510a66cf87
Ahmed Negm <91202410+AhmedNegm5@users.noreply.github.com>
2023-04-04 14:15:39 +0200

    Negm solution A

---
+++ Assiut-Ramadan-Marathons/A.Ramadan-Kareem/Negm.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define OO 2'000'000'000
+#define ull unsigned long long
+#define nl '\n'
+#define sz(x) (ll)(x.size())
+#define all(x) x.begin(),x.end()
+#define rall(s)  s.rbegin(), s.rend()
+#define getline(s) getline(cin>>ws,s)
+#define ceill(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define pi  3.141592653589793
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define multi_ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+ 
+void Fast_IO(){
+ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+// freopen("filename.in", "r", stdin);
+// freopen("filename.txt", "w", stdout);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+#endif
+}
+
+
+
+
+int dx[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+
+
+
+void solve(){
+ll n; cin>>n;
+cout<<(n==2?"3id sa3eed":"Ramadan Kareem"); 
+
+
+
+
+}
+ 
+int main(){
+    Fast_IO();
+int t =1; 
+//cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}  
