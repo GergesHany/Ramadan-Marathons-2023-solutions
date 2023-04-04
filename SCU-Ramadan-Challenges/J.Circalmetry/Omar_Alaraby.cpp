3331d3f68ee20106e29df33720bebaf37d60810b
Omar_Alaraby <omar.alaraby23@gmail.com>
2023-04-04 16:14:34 +0200

    Omar_Alaraby

---
+++ SCU-Ramadan-Challenges/J.Circalmetry/Omar_Alaraby.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+void Omar_Alaraby(char * input , char * output){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen(input, "r", stdin), freopen(output, "w", stdout);
+#endif
+}
+ 
+#define cin2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define cout2d(vec , n , m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+#define cout_map(mp) for(auto& [first, second] : mp) cout << first << " --> " << second << "\n";
+#define put(s) return void(cout << s << dl);
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define ll long long
+#define ull unsigned long long
+#define dl "\n"
+#define ordered_set tree<ll ,  null_type ,  less_equal<> ,  rb_tree_tag ,  tree_order_statistics_node_update>
+ 
+const ll Mod = 1e9 + 7;
+ 
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+ 
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) {
+    for (const T &x: v) out << x << ' ';
+    return out;
+}
+ 
+const double pi = 3.141592654;
+ 
+void Solve(){
+ 
+    double x , r; cin >> x >> r;
+ 
+    double circle = pi * r * r , traingle = .5 * r * sqrt(pow(x + r , 2) - r * r);
+    double angle = asin(sqrt(pow(x + r , 2) - r * r) / (x + r)) * 180 / pi + 180;
+ 
+    cout << fixed(6) << circle + traingle - (angle / 360 * pi * r * r) << dl;
+}
+ 
+int main(){
+    Omar_Alaraby("input.txt" , "output.txt");
+ 
+    int tc = 1;
+    cin >> tc;
+ 
+    for(int i=1; i<=tc; i++){
+        //cout << "Scenario #" << i << ":" << dl;
+        Solve();
+    }
+ 
+    Time
+    return 0;
+}
