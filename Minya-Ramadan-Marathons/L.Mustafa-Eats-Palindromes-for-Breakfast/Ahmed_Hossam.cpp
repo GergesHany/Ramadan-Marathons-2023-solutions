1c4bc1263ffe2ca83637ec5762b413acfdf8caff
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-04-07 23:55:46 +0200

    Add L Solution in Minya

---
+++ Minya-Ramadan-Marathons/L.Mustafa-Eats-Palindromes-for-Breakfast/Ahmed_Hossam.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+void Solve(){
+    // Declare a string variable named s and get its value from the user.
+    string s;
+    cin >> s;
+
+    // Declare a vector of integers named cnt with size 26.
+    vector < int > cnt(26);
+
+    // Iterate over each character in the string s and increment the corresponding
+    // integer in the vector cnt by 1.
+    for(auto &c : s) 
+        cnt[c - 'a']++;
+
+    // Declare an integer variable named len and initialize it to 0.
+    int len = 0;
+
+    // Iterate over each integer in the vector cnt and update len to be the maximum value.
+    for(auto &x : cnt) 
+        len = max(len, x);
+
+    // Output -1 if len is less than 2, otherwise output len.
+    cout << (len < 2 ? -1 : len) << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";
+        Solve();
+    }
+    return 0;
+}
