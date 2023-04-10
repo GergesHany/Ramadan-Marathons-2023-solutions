cfd35333b131ed465198b2953cbbf87421580a86
Gerges Hany <gergeshany179@gmail.com>
2023-04-10 09:06:44 +0200

    Gerges Hany solution

---
+++ SCU-Ramadan-Challenges/P. Stealing In Ramadan/GergesHany.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std; 
+using namespace __gnu_pbds;
+
+#define EPS 1e-8
+#define sz(s) (ll)(s.size())  
+#define rall(s)  s.rbegin(), s.rend()
+#define TC int t; cin >> t; while(t--)
+#define getline(s) getline(cin >> ws, s)
+#define all(vec)  vec.begin(),  vec.end()
+#define updmin(a, b) a = min((ll)a, (ll)b)
+#define updmax(a, b) a = max((ll)a, (ll)b)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define endd(s) return void(cout << s << "\n")
+#define fixed(n) cout << fixed << setprecision(n)
+#define ceill(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define cin_2d(vec, n, m) for(int i=0;i<n;i++) for(int j=0;j<m&&cin>>vec[i][j];j++);
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define ordered_set tree<ll, null_type, less <ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define multi_ordered_set tree<ll, null_type, less_equal <ll>, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+const ll INF = 1LL << 60;
+typedef unsigned long long ull;
+
+
+void Gerges_Hany(){
+  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  #ifndef ONLINE_JUDGE  
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+  #endif
+}
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x; return in;
+}
+ 
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; return out;
+}
+
+void Accepted(){  
+
+  ll n, m, k;
+  cin >> n >> m >> k;
+  vector < ll > vec(n);
+  cin >> vec;
+
+  ll ans = 0;
+  vector < ll > left[41], right[41];
+  for (int i = 0; i < (1 << (n / 2)); i++){
+    ll sum = 0;
+    for (int j = 0; j < n / 2; j++)
+      if (i & (1 << j)) sum += vec[j];
+    left[__builtin_popcount(i)].push_back(sum);
+  }
+
+  for (int i = 0; i < (1 << (n - n / 2)); i++){
+    ll sum = 0;
+    for (int j = 0; j < n - n / 2; j++)
+      if (i & (1 << j)) sum += vec[n / 2 + j];
+    right[__builtin_popcount(i)].push_back(sum);
+  }
+
+  for (int i = 0; i <= 40; i++) sort(all(left[i])), sort(all(right[i]));
+
+  for (int i = 0; i <= 40; i++){
+    for (int j = 0; j <= 40; j++){
+      if (i + j > m) break;
+      for (auto x : left[i]){
+        ll need = k - x;
+        if (need < 0) continue;
+        ans += upper_bound(all(right[j]), need) - right[j].begin();
+      }
+    }
+  }
+
+  cout << ans << "\n";
+
+}
+
+int main()
+{ 
+  
+  Gerges_Hany();
+  int testcaces = 1;
+  // cin >> testcaces;
+  while (testcaces--){  
+    // cout << "Case #" << T++ << ": ";
+    Accepted();
+  }   
+  Time;
+  return 0;
+}
